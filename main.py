bd535c3fcd66ae4d5f77a863460c3d024b8b2e4a
realkarthiknair <knairothera@gmail.com>
2022-02-03 03:22:21 +0530

    more code

--- main.py
+++ main.py
@@ -4,21 +4,26 @@ import os
 
 # find type of OS
 def default_path(platform):
+
     if platform=="nt":
         return os.environ['USERPROFILE']
+
     else:
         return os.environ['HOME']
         
 
 # fetch path from user for input and output files' location
 def getpath(prompt):
+    
     # set default path incase user skips path
     default = default_path(os.name)
+
     try:
         path=input(f'''{prompt}''')
         if not os.path.exists(path):
             raise Exception
         return path
+
     except:
         print(f'''Input skipped or entered path is invalid! 
         Choosing {default} as the path...''')
@@ -26,16 +31,28 @@ def getpath(prompt):
 
 # read files and save as md file 
 def generate_md(input_path, output_path, language):
+
+    default = default_path(os.name)
+
     input_prompt=f'''Enter directory path where files exist
     Skipping would choose {default}'''
     input_path=getpath(input_prompt)
-    ext=input("Enter file extension (e.g., .c, .py, .js, etc): ")
+
+    ext=input("Enter file extension (e.g. .c, .py, .js, etc): ")
     files=[]
     for i in os.listdir(input_path):
         if i.endswith(ext):
             files.append(i)
-
-    output_prompt=f'''Enter folder path to save file
+    
+    output_prompt=f'''Enter folder path to save md file
         Skip to choose the default path as {default}
         Enter here > '''
+    
+    output_filename=input('''Enter filename to be saved (e.g. programs.md) 
+    Enter here: ''')
+    output_path=getpath(output_prompt)
 
+    for i in files:
+        path=os.path.join(input_path, i)
+        for i in path.readlines(): 
+            
